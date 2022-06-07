d217c6384a9c42831f9070f5411d0816faad3bf1
Nate Donze <nate@fullstory.com>
2022-06-07 10:34:22 -0500

    Add FullStory cloud mode destination

---
+++ packages/destination-actions/src/destinations/fullstory/index.ts
@@ -0,0 +1,63 @@
+import type { DestinationDefinition } from '@segment/actions-core'
+import { defaultValues } from '@segment/actions-core'
+import type { Settings } from './generated-types'
+import identifyUser from './identifyUser'
+import { requestParams } from './request-params'
+
+const destination: DestinationDefinition<Settings> = {
+  name: 'Fullstory (Actions)',
+  slug: 'actions-fullstory',
+  mode: 'cloud',
+  presets: [
+    {
+      name: 'Identify User',
+      subscribe: 'type = "identify"',
+      partnerAction: 'identifyUser',
+      mapping: defaultValues(identifyUser.fields)
+    }
+  ],
+  authentication: {
+    scheme: 'custom',
+    fields: {
+      apiKey: {
+        label: 'API Key',
+        description: '[FullStory API key](https://help.fullstory.com/hc/en-us/articles/360052021773-Managing-API-Keys)',
+        type: 'password',
+        required: true
+      },
+      region: {
+        label: 'Data Region',
+        description: 'The region where your FullStory organization is provisioned.',
+        type: 'string',
+        format: 'text',
+        choices: [
+          {
+            label: 'North America',
+            value: 'north_america'
+          },
+          {
+            label: 'Europe',
+            value: 'europe'
+          }
+        ],
+        default: 'north_america'
+      }
+    },
+
+    testAuthentication: (request, { settings }) => {
+      const { url, options } = requestParams(settings).testAuthentication()
+      return request(url, options)
+    }
+  },
+
+  onDelete: async (request, { settings, payload }) => {
+    const { url, options } = requestParams(settings).deleteUser(payload.userId)
+    return request(url, options)
+  },
+
+  actions: {
+    identifyUser
+  }
+}
+
+export default destination
---
+++ packages/destination-actions/src/destinations/fullstory/request-params.ts
@@ -0,0 +1,54 @@
+import type { RequestOptions } from '@segment/actions-core'
+import type { Settings } from './generated-types'
+
+type ID = string | null | undefined
+
+interface RequestParams {
+  url: string
+  options: RequestOptions
+}
+
+export const requestParams = (settings: Settings) => {
+  const baseUrl = settings.region === 'europe' ? 'https://api.eu1.fullstory.com' : 'https://api.fullstory.com'
+  const prefixedUrl = (relativeUrl: string): string => `${baseUrl}/${relativeUrl}`
+
+  const defaultRequestParams = (relativeUrl: string): RequestParams => {
+    return {
+      url: prefixedUrl(relativeUrl),
+      options: {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Basic ${settings.apiKey}`
+        }
+      }
+    }
+  }
+
+  return {
+    testAuthentication: (): RequestParams => defaultRequestParams('operations/v1?limit=1'),
+
+    identifyUser: (userId: ID, requestBody: Object): RequestParams => {
+      const defaultParams = defaultRequestParams(`users/v1/individual/${userId}/customvars`)
+      return {
+        ...defaultParams,
+        options: {
+          ...defaultParams.options,
+          method: 'post',
+          body: JSON.stringify(requestBody)
+        }
+      }
+    },
+
+    deleteUser: (userId: ID): RequestParams => {
+      const defaultParams = defaultRequestParams(`users/v1/individual/${userId}`)
+      return {
+        ...defaultParams,
+        options: {
+          ...defaultParams.options,
+          method: 'delete'
+        }
+      }
+    }
+  }
+}
